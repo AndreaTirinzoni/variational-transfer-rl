e9ae5d94cb6742a51d0fb066cc5b374c78c23270
Rafael Rodriguez <rafaelrs307@gmail.com>
2018-03-01 16:19:13 +0100

    added simple implementation of regularized LSVI

--- algorithms/linear_q_function.py
+++ algorithms/linear_q_function.py
@@ -1,6 +1,6 @@
 import numpy as np
 import features.grbf as rbf
-
+import algorithms.regularized_lsvi as lsvi
 
 class LinearQFunction:
 
@@ -30,14 +30,12 @@ class LinearQFunction:
             qs = list()
             for a in self._actions:
                 qs.append(np.dot(self._w, self._features(np.asarray((samples[i,0], a)))))
-
             t.append(samples[i, 3] + self._gamma * (max(qs)))
-
         return np.array(t)
 
     def __call__(self, state, action):
         return np.dot(self._features(np.asarray((state, action), dtype=np.float64)), self._w)
-    
+
 
 if __name__ == '__main__':
 
@@ -55,5 +53,9 @@ if __name__ == '__main__':
     for k in range(samples.shape[0]):
         samples[k, 1] = np.random.randint(0, 5)
         samples[k, 2] = np.random.randint(-10, 10)
+
+    targets = q.compute_bellman_target(samples)
+
+    print(lsvi.RegularizedLSVI.solve(f(samples[:, 0:2]), np.array((0, 0)), np.eye(2), targets))
 
     print(q.compute_bellman_target(samples))
--- algorithms/regularized_lsvi.py
+++ algorithms/regularized_lsvi.py
@@ -4,13 +4,18 @@ class RegularizedLSVI:
     """
     Implementation of a regularized (by a normal prior over the parameters of Q) value iteration method.
     """
-
     @staticmethod
     def solve(features, mean, covariance, target):
+        """
+        :param features: matrix of feature vectors.
+        :param mean: np.ndarray mean of the prior gaussian over the parameters
+        :param covariance: np.ndarray covariance matrix of the prior gaussian over parameters
+        :param target: np.ndarray with the target values for the fitting
+        :return:
+        """
         assert features.shape[1] == mean.size
         assert covariance.shape == (mean.size, mean.size)
         assert target.shape[0] == features.shape[0]
-
-        prec = np.invert(covariance)
+        prec = np.linalg.inv(covariance)
         X = np.dot(features.T, features)
-        return np.dot(np.invert(X + prec), np.dot(features.T, target) + np.dot(prec, mean))
+        return np.dot(np.linalg.inv(X + prec), np.dot(features.T, target) + np.dot(prec, mean))
