fixed bugs in target computation

algorithms/linear_q_function.py
@@ -5,7 +5,7 @@ import utils
 
 class LinearQFunction:
 
-    def __init__(self, actions, features, params=None, state_dim=1, action_dim=1, gamma=1):
+    def __init__(self, actions, features, params=None, state_dim=1, action_dim=1, gamma=0.9):
         """
         :param actions: tuple with the possible actions available
         :param features: feature object
@@ -38,7 +38,7 @@ class LinearQFunction:
             qs = list()
             for a in self.actions:
                 qs.append(np.dot(self._w, self._features(np.hstack((samples[i, s_prime: s_prime + self._state_dim], a)))))
-            t.append((samples[i, r] + self._gamma * (max(qs))) if not samples[i, -1] else samples[i, r])
+            t.append((samples[i, r] + self._gamma * max(qs)) if samples[i, -1] == 0 else samples[i, r])
         return np.array(t)
 
     def update_weights(self, w):

algorithms/regularized_lsvi.py
@@ -22,6 +22,6 @@ class RegularizedLSVI:
             prec = np.linalg.inv(covariance)
 
         X = np.dot(features.T, features)
+        print(X)
         Y = (np.dot(features.T, target) + np.dot(prec, mean)) if prior else np.dot(features.T, target)
-        print("det X = " + str(np.linalg.det(X)))
         return np.dot(np.linalg.inv(X + prec) if prior else np.linalg.inv(X), Y)

algorithms/simple_rl.py
@@ -9,6 +9,7 @@ import features.grbf as grbf
 
 """
 Implementation of a simple RL algorithm based on an e-greedy policy and a linearly parameterized Q function
+without the prior distribution.
 """
 def simple_RL(mdp, Q, epsilon=0, K=1, batch_size=1, render=False):
     pol = policy.eGreedyPolicy(Q, Q.actions, epsilon)
@@ -16,8 +17,9 @@ def simple_RL(mdp, Q, epsilon=0, K=1, batch_size=1, render=False):
     for i in range(K):
         samples = utils.generate_episodes(mdp, pol, batch_size, render=render)
         targets = Q.compute_bellman_target(samples[:, 1:])
-        w = lsvi.RegularizedLSVI.solve(Q.compute_features(samples), targets, prior=False)
+        w = lsvi.RegularizedLSVI.solve(Q.compute_features(samples[:, 1:]), targets, prior=False)
         Q.update_weights(w)
+    
         r.append(np.sum(samples[:, 1+mdp.state_dim+mdp.action_dim])/batch_size)
     return r
 
@@ -26,13 +28,13 @@ if __name__ == '__main__':
     n = 5
     acts = 4
     k = (n-1)*(n-1)*acts
-    world = env.WalledGridworld(np.array((n, n)))
+    world = env.WalledGridworld(np.array((n, n)), door_x=n/2)
     mean = np.array([[x, y, a] for x in range(1, n) for y in range(1, n) for a in range(acts)])
     variance = (np.ones(k)/3)**2
     features = grbf.GaussianRBF(mean, variance, K=k, dims=world.state_dim+world.action_dim)
     q = Q.LinearQFunction(range(acts), features, np.zeros(k), state_dim=world.state_dim, action_dim=world.action_dim)
 
-    for i in range(100):
+    for i in range(200):
         print("Iteration " + str(i))
         r = simple_RL(world, q, epsilon=0.1, render=True)
         print(r)