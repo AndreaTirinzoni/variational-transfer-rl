64169589a36458f88313357ee4c2a3d5b10b86df
AndreaTirinzoni <andrea.tirinzoni@polimi.it>
2018-03-08 18:37:35 +0100

    Added plot_Q function

--- scripts/run_nfqi.py
+++ scripts/run_nfqi.py
@@ -6,6 +6,30 @@ from algorithms.nn_q_function import NNQ
 import matplotlib.pyplot as plt
 
 
+def plot_Q(Q):
+
+    V = [[], [], [], []]
+    X = np.arange(0.0, 5.1, 0.1)
+    for x in X:
+        for y in X:
+            vals = Q.compute_all_actions(np.array([x, y]))
+            V[0].append(vals[0])
+            V[1].append(vals[1])
+            V[2].append(vals[2])
+            V[3].append(vals[3])
+    V = [np.flip(np.array(v).reshape(X.size, X.size), axis=0) for v in V]
+    fig, ax = plt.subplots(2, 2)
+    ax[0, 0].imshow(V[0], cmap="hot", interpolation="gaussian")
+    ax[0, 0].set_title("UP")
+    ax[0, 1].imshow(V[1], cmap="hot", interpolation="gaussian")
+    ax[0, 1].set_title("RIGHT")
+    ax[1, 0].imshow(V[2], cmap="hot", interpolation="gaussian")
+    ax[1, 0].set_title("DOWN")
+    ax[1, 1].imshow(V[3], cmap="hot", interpolation="gaussian")
+    ax[1, 1].set_title("LEFT")
+    plt.show()
+
+
 def solve_task(mdp, Q, epsilon=0.2, n_iter=20, n_fit=1, batch_size=1, render=False, verbose=False):
     pi_exp = policy.eGreedyPolicy(Q, Q.actions, epsilon)
     pi_eval = policy.eGreedyPolicy(Q, Q.actions, 0)
@@ -27,6 +51,9 @@ def solve_task(mdp, Q, epsilon=0.2, n_iter=20, n_fit=1, batch_size=1, render=Fal
         for k in range(n_fit):
             y = Q.compute_bellman_target(r, s_prime, absorbing)
             Q.fit(s, a, y)
+
+        if verbose:
+            plot_Q(Q)
 
         rewards.append(utils.evaluate_policy(mdp, pi_eval, initial_states=[np.array([0., 0.]) for _ in range(5)],render=render)[0])
         if verbose:
