b9e887466c8335aa40ca38add3c0a28185d30362
AndreaTirinzoni <andrea.tirinzoni@polimi.it>
2018-04-13 10:29:23 +0200

    Modified plot script

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -51,8 +51,8 @@ def plot_curves(x_data, y_mean_data, y_std_data=None, title="", x_label="Episode
     plt.show()
 
 
-files = ["nt_mgw5x5_basis6_xi0.5_batch100_iter300_eps0.2", "vt_mgw5x5_basis6_xi0.5_batch100_iter300"]
-names = ["No Transfer", "Transfer"]
+files = ["nt_gw5x5","vt_gw5x5_lambda0.001_tc0","vt_gw5x5_lambda0.0001_tc0","vt_gw5x5_lambda0.001_tc1"]
+names = ["No Transfer", "Transfer (0.001)", "Transfer (0.0001)", "Transfer (TC)"]
 
 x = []
 y_mean = []
@@ -63,39 +63,49 @@ y3_mean = []
 y3_std = []
 y4_mean = []
 y4_std = []
+y5_mean = []
+y5_std = []
 
 for file in files:
     results = [r[2] for r in utils.load_object(file)]
     iterations = []
+    episodes = []
     n_samples = []
-    rewards = []
+    lear_rew = []
+    eval_rew = []
     l_2 = []
     l_inf = []
     sft = []
     for result in results:
         iterations.append(result[0])
-        n_samples.append(result[1])
-        rewards.append([r[0] for r in result[2]])
-        l_2.append(result[3])
-        l_inf.append(result[4])
-        sft.append(result[5])
+        episodes.append(result[1])
+        n_samples.append(result[2])
+        lear_rew.append(result[3])
+        eval_rew.append(result[4])
+        l_2.append(result[5])
+        l_inf.append(result[6])
+        sft.append(result[7])
     iterations = np.array(iterations)
     x.append(np.mean(iterations,axis=0))
     n_samples = np.array(n_samples)
-    rewards = np.array(rewards)
-    y_mean.append(np.mean(rewards, axis=0))
-    y_std.append(2 * np.std(rewards, axis=0) / np.sqrt(rewards.shape[0]))
+    lear_rew = np.array(lear_rew)
+    y_mean.append(np.mean(lear_rew, axis=0))
+    y_std.append(2 * np.std(lear_rew, axis=0) / np.sqrt(lear_rew.shape[0]))
+    eval_rew = np.array(eval_rew)
+    y2_mean.append(np.mean(eval_rew, axis=0))
+    y2_std.append(2 * np.std(eval_rew, axis=0) / np.sqrt(eval_rew.shape[0]))
     l_2 = np.array(l_2)
-    y2_mean.append(np.mean(l_2, axis=0))
-    y2_std.append(2 * np.std(l_2, axis=0) / np.sqrt(l_2.shape[0]))
+    y3_mean.append(np.mean(l_2, axis=0))
+    y3_std.append(2 * np.std(l_2, axis=0) / np.sqrt(l_2.shape[0]))
     l_inf = np.array(l_inf)
-    y3_mean.append(np.mean(l_inf, axis=0))
-    y3_std.append(2 * np.std(l_inf, axis=0) / np.sqrt(l_inf.shape[0]))
+    y4_mean.append(np.mean(l_inf, axis=0))
+    y4_std.append(2 * np.std(l_inf, axis=0) / np.sqrt(l_inf.shape[0]))
     sft = np.array(sft)
-    y4_mean.append(np.mean(sft, axis=0))
-    y4_std.append(2 * np.std(sft, axis=0) / np.sqrt(sft.shape[0]))
+    y5_mean.append(np.mean(sft, axis=0))
+    y5_std.append(2 * np.std(sft, axis=0) / np.sqrt(sft.shape[0]))
 
-plot_curves(x, y_mean, y_std, title="", x_label="Iterations", y_label="Reward", names=names, file_name="rew")
-plot_curves(x, y2_mean, y2_std, title="", x_label="Iterations", y_label="L_2", names=names, file_name="l2")
-plot_curves(x, y3_mean, y3_std, title="", x_label="Iterations", y_label="L_INF", names=names, file_name="linf")
-plot_curves(x, y4_mean, y4_std, title="", x_label="Iterations", y_label="Softmax Error", names=names, file_name="sft")
+plot_curves([a[1:] for a in x], [a[1:] for a in y_mean], [a[1:] for a in y_std], title="", x_label="Iterations", y_label="Learning Reward", names=names, file_name="lrev")
+plot_curves(x, y2_mean, y2_std, title="", x_label="Iterations", y_label="Evaluation Reward", names=names, file_name="erew")
+plot_curves(x, y3_mean, y3_std, title="", x_label="Iterations", y_label="L_2", names=names, file_name="l2")
+plot_curves(x, y4_mean, y4_std, title="", x_label="Iterations", y_label="L_INF", names=names, file_name="linf")
+plot_curves(x, y5_mean, y5_std, title="", x_label="Iterations", y_label="Softmax Error", names=names, file_name="sft")
