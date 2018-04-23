436b458b5007a53a996672b8084654ba95cd0b63
AndreaTirinzoni <andrea.tirinzoni@polimi.it>
2018-04-23 09:40:35 +0200

    Implemented run_vt_nn

--- scripts/run_vt_nn.py
+++ scripts/run_vt_nn.py
@@ -2,10 +2,8 @@ import sys
 sys.path.append("../")
 
 import numpy as np
-from envs.walled_gridworld import WalledGridworld
-from envs.marcellos_gridworld import MarcellosGridworld
-from features.agrbf import build_features_gw
-from approximators.linear import LinearQFunction
+from envs.cartpole import CartPoleEnv
+from approximators.mlp import MLPQFunction
 from operators.mellow import MellowBellmanOperator
 from policies import EpsilonGreedy
 import utils
@@ -77,12 +75,16 @@ def run(mdp, seed=None):
     if seed is not None:
         np.random.seed(seed)
 
-    # Build the features
-    features = build_features_gw(gw_size, n_basis, n_actions, state_dim, action_dim)
     # Create BellmanOperator
     operator = MellowBellmanOperator(kappa, tau, xi, gamma, state_dim, action_dim)
     # Create Q Function
-    Q = LinearQFunction(features, np.arange(n_actions), state_dim, action_dim)
+    layers = [l1]
+    if l2 > 0:
+        layers.append(l2)
+    Q = MLPQFunction(state_dim, n_actions, layers=layers)
+    # Set number of weights
+    global K
+    K = Q._nn.num_weights
     # Initialize policies
     pi_u = EpsilonGreedy(Q, Q.actions, epsilon=1)
     pi_g = EpsilonGreedy(Q, Q.actions, epsilon=0)
@@ -132,11 +134,11 @@ def run(mdp, seed=None):
         # If we do not use time coherent exploration, resample parameters
         Q._w = sample_posterior(params) if not time_coherent else Q._w
         # Take greedy action wrt current Q-function
-        a = np.array([np.argmax(Q.value_actions(s))])
+        a = np.argmax(Q.value_actions(s))
         # Step
         s_prime, r, done, _ = mdp.step(a)
         # Build the new sample and add it to the dataset
-        dataset = utils.add_sample(dataset, buffer_size, h, s, a, r, s_prime, done)
+        dataset = utils.add_sample(dataset, buffer_size, h, s, np.array([a]), r, s_prime, done)
 
         # Take a step of gradient if needed
         if i % train_freq == 0:
@@ -171,7 +173,7 @@ def run(mdp, seed=None):
             mu, _ = unpack(params)
             Q._w = mu
             # utils.plot_Q(Q)
-            rew = utils.evaluate_policy(mdp, pi_g, render=render, initial_states=eval_states)[0]
+            rew = utils.evaluate_policy(mdp, pi_g, render=render, n_episodes=n_eval_episodes)[0]
             learning_rew = np.mean(episode_rewards[-mean_episodes - 1:-1]) if len(episode_rewards) > 1 else 0.0
             br = operator.bellman_residual(Q, dataset) ** 2
             l_2_err = np.average(br)
@@ -203,7 +205,7 @@ def run(mdp, seed=None):
     run_info = [iterations, episodes, n_samples, learning_rewards, evaluation_rewards, l_2, l_inf, sft, fvals]
     weights = np.array(mu)
 
-    return [mdp.door_x, weights, run_info]
+    return [(mdp.masscart,mdp.masspole,mdp.length), weights, run_info]
 
 
 # Global parameters
@@ -219,26 +221,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--kappa", default=100.)
 parser.add_argument("--xi", default=0.5)
 parser.add_argument("--tau", default=0.0)
-parser.add_argument("--batch_size", default=100)
+parser.add_argument("--batch_size", default=50)
 parser.add_argument("--max_iter", default=5000)
 parser.add_argument("--buffer_size", default=10000)
 parser.add_argument("--random_episodes", default=0)
 parser.add_argument("--train_freq", default=1)
-parser.add_argument("--eval_freq", default=50)
-parser.add_argument("--mean_episodes", default=50)
+parser.add_argument("--eval_freq", default=100)
+parser.add_argument("--mean_episodes", default=20)
+parser.add_argument("--l1", default=64)
+parser.add_argument("--l2", default=0)
 parser.add_argument("--alpha", default=0.001)
 parser.add_argument("--lambda_", default=0.001)
 parser.add_argument("--time_coherent", default=False)
-parser.add_argument("--n_weights", default=100)
-parser.add_argument("--n_source", default=10)
+parser.add_argument("--n_weights", default=10)
+parser.add_argument("--n_source", default=50)
 parser.add_argument("--sigma_reg", default=0.0001)
 parser.add_argument("--cholesky_clip", default=0.0001)
-parser.add_argument("--env", default="two-room-gw")
-parser.add_argument("--gw_size", default=5)
-# Door at -1 means random positions over all runs
-parser.add_argument("--door", default=-1)
-parser.add_argument("--door2", default=-1)
-parser.add_argument("--n_basis", default=6)
+parser.add_argument("--env", default="cartpole")
+# Cartpole parameters (default = randomize
+parser.add_argument("--cart_mass", default=-1)
+parser.add_argument("--pole_mass", default=-1)
+parser.add_argument("--pole_length", default=-1)
 parser.add_argument("--n_jobs", default=1)
 parser.add_argument("--n_runs", default=1)
 parser.add_argument("--file_name", default="gvt_{}".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S")))
@@ -256,6 +259,8 @@ random_episodes = int(args.random_episodes)
 train_freq = int(args.train_freq)
 eval_freq = int(args.eval_freq)
 mean_episodes = int(args.mean_episodes)
+l1 = int(args.l1)
+l2 = int(args.l2)
 alpha = float(args.alpha)
 lambda_ = float(args.lambda_)
 time_coherent = bool(args.time_coherent)
@@ -264,26 +269,23 @@ n_source = int(args.n_source)
 sigma_reg = float(args.sigma_reg)
 cholesky_clip = float(args.cholesky_clip)
 env = str(args.env)
-gw_size = int(args.gw_size)
-door = float(args.door)
-door2 = float(args.door2)
-n_basis = int(args.n_basis)
+cart_mass = float(args.cart_mass)
+pole_mass = float(args.pole_mass)
+pole_length = float(args.pole_length)
 n_jobs = int(args.n_jobs)
 n_runs = int(args.n_runs)
 file_name = str(args.file_name)
 source_file = str(args.source_file)
 
-# Number of features
-K = n_basis ** 2 * n_actions
+# Number of weights
+K = 0
 
 # Generate tasks
-doors = [np.random.uniform(0.5, gw_size - 0.5) if door < 0 else door for _ in range(n_runs)]
-doors2 = [np.random.uniform(0.5, gw_size - 0.5) if door2 < 0 else door2 for _ in range(n_runs)]
-if env == "two-room-gw":
-    mdps = [WalledGridworld(np.array([gw_size, gw_size]), door_x=d) for d in doors]
-elif env == "three-room-gw":
-    mdps = [MarcellosGridworld(np.array([gw_size, gw_size]), door_x=(d1,d2)) for (d1,d2) in zip(doors,doors2)]
-eval_states = [np.array([0., 0.]) for _ in range(10)]
+mc = [np.random.uniform(0.5, 1.5) if cart_mass < 0 else cart_mass for _ in range(n_runs)]
+mp = [np.random.uniform(0.1, 0.2) if pole_mass < 0 else pole_mass for _ in range(n_runs)]
+l = [np.random.uniform(0.2, 0.8) if pole_length < 0 else pole_length for _ in range(n_runs)]
+mdps = [CartPoleEnv(a,b,c) for a,b,c in zip(mc,mp,l)]
+n_eval_episodes = 5
 
 if n_jobs == 1:
     results = [run(mdp) for mdp in mdps]
